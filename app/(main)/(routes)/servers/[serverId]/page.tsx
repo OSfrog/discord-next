794c3aca538e189015a4bb5321c589ff2e610717
OSfrog <tommy_8786@hotmail.com>
2023-09-11 11:27:58 +0200

    feat: redirect user to general channel when connected to a server

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,9 +1,51 @@
-const ServerIdPage = () => {
-  return (
-    <div>
-      ServerPage
-    </div>
-  );
+// An example of a React Server Component
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+  params: { serverId: string };
+}
+
+const ServerIdPage: React.FC<ServerIdPageProps> = async (
+  { params: { serverId } },
+) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "General",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "General") {
+    return null;
+  }
+
+  return redirect(`/servers/${serverId}/channels/${initialChannel.id}`);
 };
 
 export default ServerIdPage;
